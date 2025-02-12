258c5bb04a9614151210cf915be6bcfb6ec6bb0a
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-12 17:12:42 +0000

    add server actions allowed origins config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const DOMAIN_DEV = "silver-train-rxrgrxxx6r92vpq-3000.app.github.dev"
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [DOMAIN_DEV, "localhost:3000"]
+    }
+  }
 };
 
 export default nextConfig;
