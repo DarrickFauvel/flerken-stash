cedddfef1bf7038a5564f9a110f1ddec391af0fc
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-02-12 17:13:46 +0000

    create new user page form

---
+++ app/users/create/page.tsx
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import Form from "next/form";
+
+type RawFormDataProps = {
+  name: string
+  email: string
+}
+
+export default function NewUserPage() {
+  async function createUser(formData: FormData) {
+    "use server";
+
+    const rawFormData = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+    };
+
+    console.log(rawFormData);
+    const user = await prisma.user.create({
+      data: rawFormData,
+    });
+    const allUsers = await prisma.user.findMany();
+
+    console.log(allUsers);
+
+    // mutate data
+    // revalidate cache
+  }
+
+  return (
+    <>
+      <h1>Create New User</h1>
+      <Form action={createUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <button type="submit">Create</button>
+      </Form>
+    </>
+  );
+}
